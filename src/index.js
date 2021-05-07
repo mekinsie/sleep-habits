43ac63cbac948519af39eb64bf3ea9c93d20385c
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-05-07 10:36:40 -0700

    Import and create redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import {createStore} from 'redux';
+import rootReducer from './reducers/index.js';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
