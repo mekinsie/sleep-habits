f5c1c299e3412e9c092ecf23cebee4b69341ab6d
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-05-07 12:24:36 -0700

    Create newSleepForm component

---
+++ src/components/NewSleepForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+import { useFirestore } from 'react-redux-firebase'
+import FadeIn from 'react-fade-in';
+
+function NewSleepForm(props){
+  const firestore = useFirestore();
+
+  function addSleepToFirestore(event){
+    event.preventDefault();
+    props.onNewSleepCreation();
+    return firestore.collection('sleepData').add(
+      {
+        date: event.target.date.value,
+        bedTime: event.target.bedTime.value,
+        wakeTime: event.target.wakeTime.value,
+        energyLevel: event.target.energyLevel.value,
+        mood: event.target.mood.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    )
+  }
+
+  return(
+    <React.Fragment>
+      <FadeIn>
+        <ReusableForm
+        formSubmissionHandler={addSleepToFirestore}
+        buttonText="Save Sleep" />
+      </FadeIn>
+    </React.Fragment>
+  );
+}
+
+NewSleepForm.propTypes = {
+  OnNewSleepCreation: PropTypes.func
+}
+
+export default NewSleepForm;
