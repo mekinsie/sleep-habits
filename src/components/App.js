d98f3db43620642a8ee627e3d79326173c7001b3
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-05-11 13:15:06 -0700

    Add nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SleepControl from './SleepControl';
-
+import Nav from './Nav';
 function App() {
   return (
     <React.Fragment>
-      <div class="container">
-      <div class="moving_shape"></div>
+      <Nav />
+      <div className="container">
+      <div className="moving_shape"></div>
       </div>
       <SleepControl />
     </React.Fragment>
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav(){
+  return (
+    <>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Options</a></li>
+      <li><a href="#">Logout</a></li>
+    </ul>
+    </>
+  );
+}
+
+export default Nav;
